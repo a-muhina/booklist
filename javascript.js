38293348d535aaa1250aa02ac9e40df0b387196a
DesertLurker <alexandragmuhina@gmail.com>
2024-06-24 15:14:45 +0300

    make the list persistent

--- javascript.js
+++ javascript.js
@@ -7,6 +7,8 @@ const booklist = document.getElementById('booklist');
 
 let books = JSON.parse(localStorage.getItem('books')) || [];
 
+
+
 class Book {
     constructor(title, author, isbn) {
         this._title = title;
@@ -26,6 +28,33 @@ class Book {
         return this._isbn;
     }
 }
+
+books = books.map(book => new Book(book._title, book._author, book._isbn));
+
+const addBookToList = book => {
+    let newBook = document.createElement('tr');
+    newBook.classList.add('book');
+
+    let newTitle = document.createElement('td');
+    newTitle.classList.add('title');
+    newTitle.innerHTML = book.title;
+    newBook.appendChild(newTitle);
+
+    let newAuthor = document.createElement('td');
+    newAuthor.classList.add('author');
+    newAuthor.innerHTML = book.author;
+    newBook.appendChild(newAuthor);
+
+    let newISBN = document.createElement('td');
+    newISBN.classList.add('isbn');
+    newISBN.innerHTML = book.isbn;
+    newBook.appendChild(newISBN);
+
+    booklist.appendChild(newBook);
+}
+
+books.forEach(book => addBookToList(book));
+
 let handler = (event) => {
     event.preventDefault();
 
@@ -36,27 +65,10 @@ let handler = (event) => {
     let book = new Book(title, author, isbn);
 
     books.push(book);
+
     localStorage.setItem('books', JSON.stringify(books));
 
-    let newBook = document.createElement('tr');
-    newBook.class = 'book';
-
-    let newTitle = document.createElement('td');
-    newTitle.class = 'title';
-    newTitle.innerHTML = book.title;
-    newBook.appendChild(newTitle);
-
-    let newAuthor = document.createElement('td');
-    newAuthor.class = 'author';
-    newAuthor.innerHTML = book.author;
-    newBook.appendChild(newAuthor);
-
-    let newISBN = document.createElement('td');
-    newISBN.class = 'isbn';
-    newISBN.innerHTML = book.isbn;
-    newBook.appendChild(newISBN);
-
-    booklist.appendChild(newBook);
+    addBookToList(book);
 
     title.value = '';
     author.value = '';
