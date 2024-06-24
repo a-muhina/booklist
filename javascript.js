dfdd8a9d721ebc9e8637d7596ddf6d0c004228d3
DesertLurker <alexandragmuhina@gmail.com>
2024-06-24 18:50:51 +0300

    add delete button

--- javascript.js
+++ javascript.js
@@ -7,8 +7,6 @@ const booklist = document.getElementById('booklist');
 
 let books = JSON.parse(localStorage.getItem('books')) || [];
 
-
-
 class Book {
     constructor(title, author, isbn) {
         this._title = title;
@@ -31,32 +29,69 @@ class Book {
 
 books = books.map(book => new Book(book._title, book._author, book._isbn));
 
+
+/*Deletion*/
+let deletion = (newBook, book) => {
+    let deleteTD = document.createElement('td');
+    deleteTD.classList.add('button');
+
+    let deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete');
+
+    deleteTD.appendChild(deleteButton);
+    newBook.appendChild(deleteTD);
+
+    deleteButton.onclick = () => {
+        books = books.filter(b => b.isbn !== book.isbn);
+        localStorage.setItem('books', JSON.stringify(books));
+        newBook.remove();
+    }
+}
+
+/*Visualization*/
 const addBookToList = book => {
     let newBook = document.createElement('tr');
     newBook.classList.add('book');
 
     let newTitle = document.createElement('td');
-    newTitle.classList.add('title');
+    newTitle.classList.add('text');
     newTitle.innerHTML = book.title;
     newBook.appendChild(newTitle);
 
     let newAuthor = document.createElement('td');
-    newAuthor.classList.add('author');
+    newAuthor.classList.add('text');
     newAuthor.innerHTML = book.author;
     newBook.appendChild(newAuthor);
 
     let newISBN = document.createElement('td');
-    newISBN.classList.add('isbn');
+    newISBN.classList.add('text');
     newISBN.innerHTML = book.isbn;
     newBook.appendChild(newISBN);
+
+    /*let editTD = document.createElement('td');
+    editTD.classList.add('button');
+
+    let editButton = document.createElement('button');
+    editButton.classList.add('edit');
+
+    editTD.appendChild(editButton);
+    newBook.appendChild(editTD);*/
+
+    deletion(newBook, book);
 
     booklist.appendChild(newBook);
 }
 
 books.forEach(book => addBookToList(book));
 
-let handler = (event) => {
+
+/*Adding an entry*/
+let entry = event => {
     event.preventDefault();
+    const remove = document.getElementById('toremove');
+    if (remove) {
+        booklist.removeChild(remove);
+    }
 
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
@@ -70,10 +105,11 @@ let handler = (event) => {
 
     addBookToList(book);
 
-    title.value = '';
-    author.value = '';
-    isbn.value = '';
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
 }
 
-submit.onclick = handler;
+submit.onclick = entry;
+
 
