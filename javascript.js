e41c1eb8ae735af4bff84504910483d8c609b849
DesertLurker <alexandragmuhina@gmail.com>
2024-06-25 19:26:31 +0300

    add edit buttons

--- javascript.js
+++ javascript.js
@@ -25,10 +25,21 @@ class Book {
     get isbn() {
         return this._isbn;
     }
+
+    set title(title) {
+        this._title = title;
+    }
+
+    set author(author) {
+        this._author = author;
+    }
+
+    set isbn(isbn) {
+        this._isbn = isbn;
+    }
 }
 
 books = books.map(book => new Book(book._title, book._author, book._isbn));
-
 
 /*Deletion*/
 let deletion = (newBook, book) => {
@@ -45,6 +56,72 @@ let deletion = (newBook, book) => {
         books = books.filter(b => b.isbn !== book.isbn);
         localStorage.setItem('books', JSON.stringify(books));
         newBook.remove();
+    }
+}
+
+/*Editing*/
+let editing = (newBook, book) => {
+    let editTD = document.createElement('td');
+    editTD.classList.add('button');
+
+    let editButton = document.createElement('button');
+    editButton.classList.add('edit');
+
+    let confirmButton = document.createElement('button'); 
+    confirmButton.classList.add('confirm');
+
+    editTD.appendChild(editButton);
+    newBook.appendChild(editTD);
+    
+    editButton.onclick = () => {
+        const row = editButton.parentNode.parentNode;
+        let textFields = row.querySelectorAll('td.text');
+        let editFields = row.querySelectorAll('input.editing');
+        let isEditing = editButton.classList.contains('confirm');
+
+        if (isEditing) {
+            let index = books.findIndex(i => i.isbn === book.isbn);
+            console.log(index);
+            editFields.forEach((field, idx) => {
+                    const input = field.value;
+                    field.replaceWith(input);
+                    
+                    switch (idx) {
+                        case 0:
+                            books[index].title = input;
+                            localStorage.setItem('books', JSON.stringify(books));
+                            break;
+                        case 1:
+                            books[index].author = input;
+                            localStorage.setItem('books', JSON.stringify(books));
+                            break;
+                        case 2: 
+                            books[index].isbn = input;
+                            localStorage.setItem('books', JSON.stringify(books));
+                            break;
+                    }     
+            });
+
+            localStorage.setItem('books', JSON.stringify(books));
+            
+            editButton.classList.remove('confirm');
+            editButton.classList.add('edit');
+
+        } else {
+            textFields.forEach(field => {
+                const text = field.innerHTML;
+                let wrapper = document.createElement('td');
+                wrapper.classList.add('text');
+                let editing = document.createElement('input');
+                editing.type = 'text';
+                editing.classList.add('editing');
+                editing.value = `${text}`;
+                wrapper.appendChild(editing);
+                field.replaceWith(wrapper);
+            });
+            editButton.classList.remove('edit');
+            editButton.classList.add('confirm');
+        }
     }
 }
 
@@ -68,16 +145,8 @@ const addBookToList = book => {
     newISBN.innerHTML = book.isbn;
     newBook.appendChild(newISBN);
 
-    /*let editTD = document.createElement('td');
-    editTD.classList.add('button');
-
-    let editButton = document.createElement('button');
-    editButton.classList.add('edit');
-
-    editTD.appendChild(editButton);
-    newBook.appendChild(editTD);*/
-
     deletion(newBook, book);
+    editing(newBook, book);
 
     booklist.appendChild(newBook);
 }
